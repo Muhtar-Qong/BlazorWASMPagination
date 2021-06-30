61e7ed49c820a89a024e6fcbf6055ee90c9cd207
mqnn12@yahoo.com <mqnn12@yahoo.com>
2021-06-30 17:23:57 -0400

    Add project files.

---
+++ BlazorWASMPagination/Models/PageItem.cs
@@ -0,0 +1,17 @@
+﻿namespace BlazorWASMPagination.Models
+{
+    public class PageItem
+    {
+        public PageItem(int pageIndex, bool enabled, string text)
+        {
+            PageIndex = pageIndex;
+            Enabled = enabled;
+            Text = text;
+        }
+
+        public string Text { get; set; }
+        public int PageIndex { get; set; }
+        public bool Enabled { get; set; } = true;
+        public bool Active { get; set; } = false;
+    }
+}
